e58854a9de58459ec842401ef942246920cc3b59
Taelor Lambert <taeloralysse@gmail.com>
2023-03-11 14:49:47 -0500

    LOOPS

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+let event = "surprise"
+function writeCards(names, event){
+    let messages = []
+    for (let i = 0; i < names.length; i++){
+        messages.push("Thank you, " + names [i] + ", for the wonderful " + event + " gift!")}
+        return messages
+    }
+    let count = []
+    function countDown(count) {
+        while (count > -1){
+        console.log(count--)}
+        return count
+    }
+let newCount = []
+  function newCountDown(newCount) { 
+    while (newCount > -1){
+console.log(newCount--)}
+return newCount}
+  
+
+
+    
+       
+
+
